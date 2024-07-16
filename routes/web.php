86972d9c9f964080ab3e8871e6031f91376f2317
anggacipta <anggasusanto12@gmail.com>
2024-07-16 14:47:51 +0700

    add adminlte

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/test-lte', function () {
+    return view('dashboard/admin/index');
+});
